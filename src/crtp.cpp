d446817713fef22b1ee4319d7ad19a45e1e60fc0
stijnlemm <lemmstijn@hotmail.com>
2024-03-09 10:04:40 +0100

    [init] Traitor header with helpers

--- src/crtp.cpp
+++ src/crtp.cpp
@@ -1,67 +1,34 @@
 #include <iostream>
 #include <utility>
 #include <string>
+#include "Traitor"
 
-// macro induced trait system c++
-// todo, const qualifiers?
-
-// Create function arguments
-#define ARGS_0()            void
-#define ARGS_1()            void
-#define ARGS_2(t1,v1)        t1 v1
-#define ARGS_4(t1,v1,t2,v2)  t1 v1, t2 v2
-#define ARGS_N(_4,_3,_2,_1,_0,N,...)   ARGS##N
-#define ARGS(...)   ARGS_N(_0,##__VA_ARGS__,_4,_3,_2,_1,_0)(__VA_ARGS__)
-
-// Pass arguments to printf with a leading comma.
-#define PASS_0()
-#define PASS_2(t1,v1)       , v1        
-#define PASS_4(t1,v1,t2,v2) , v1, v2
-#define PASS_N(_4,_3,_2,_1,_0,N,...)  PASS##N
-#define PASS(...)  PASS_N(_0,##__VA_ARGS__,_4,_3,_2,_1,_0)(__VA_ARGS__)
-
-#define TRAIT(name) 											\
-template<typename Child> 										\
-struct name 												\
-
-#define TRAIT_FN(rettype, name, args...)								\
-rettype name(ARGS(__VA_ARGS__)) { return static_cast<Child&>(*this).name##_impl(PASS(__VA_ARGS__)); } 	\
-
-#define TRAIT_FN_IMPL(rettype, name, args...)								\
-rettype name##_impl(args...) 										\
-
-#define TRAIT_SFN(rettype, name, args...)								\
-static rettype name(args...) { return static_cast<Child&>(*this)::name##_impl(args); }			\
-
-#define TRAIT_SFN_IMPL(rettype, name, args...)	\
-static rettype name##_impl(args...) 		\
-
-TRAIT(VeryImportantTrait)
+trait(VeryImportantTrait)
 {
-	TRAIT_FN(bool, increment, int, type);
-	TRAIT_FN(std::string, to_string);
-	TRAIT_FN(void, sayHello, std::string name);
-	TRAIT_SFN(int, GlobalConfig);
+	trait_fn(bool, increment);
+	trait_fn(std::string, to_string);
+	trait_fn(void, sayHello, std::string, name);
+	trait_sfn(int, GlobalConfig);
 };
 
 // impl of printable trait with cat
 class Cat : public VeryImportantTrait<Cat>
 {
 public:
-	TRAIT_FN_IMPL(std::string, to_string)
+	trait_fn_impl(std::string, to_string)
 	{
 		return "Meow!";
 	}
 
-	TRAIT_FN_IMPL(void, sayHello, std::string name)
+	trait_fn_impl(void, sayHello, std::string name)
 	{
-		std::cout << "Hello from cat, to:" << name  << std::endl;
+		std::cout << "Hello from cat, to: " << name  << std::endl;
 	}
-	TRAIT_FN_IMPL(bool, increment)
+	trait_fn_impl(bool, increment)
 	{
 		return true;
 	}
-	TRAIT_SFN_IMPL(int, GlobalConfig)
+	trait_sfn_impl(int, GlobalConfig)
 	{
 		return 1u;
 	}
@@ -70,19 +37,20 @@ public:
 class Dog : public VeryImportantTrait<Dog>
 {
 public:
-	std::string to_string() const
+	trait_fn_impl(std::string, to_string)
 	{
 		return "Woof!";
 	}
-	void sayHello()
+
+	trait_fn_impl(void, sayHello, std::string name)
 	{
-		std::cout << "Hello from dog!" << std::endl;
+		std::cout << "Hello from dog, to: " << name  << std::endl;
 	}
-	TRAIT_FN_IMPL(bool, increment)
+	trait_fn_impl(bool, increment)
 	{
-		return false;
+		return true;
 	}
-	static int GlobalConfig()
+	trait_sfn_impl(int, GlobalConfig)
 	{
 		return 2u;
 	}
@@ -96,7 +64,7 @@ void do_print(VeryImportantTrait<type>& trait)
 	{
 		std::cout << "Incremented!" << std::endl;
 	}
-	trait.sayHello();
+	trait.sayHello("Caller");
 
 	std::cout << "Config: " << VeryImportantTrait<type>::GlobalConfig() << std::endl;
 }
